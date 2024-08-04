1f6b5e84a0a4f74490aa9e310a9053042b0796a2
DarttGoblin <yassine.bazgour@gmail.com>
2024-08-04 16:28:12 +0100

    WhisperLoginServerFile

---
+++ Login.js
@@ -0,0 +1,54 @@
+const mysql = require("mysql");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 7003;
+
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.removeHeader('Permissions-Policy');
+    next();
+});
+
+app.post("/", (req, res) => {
+    const usernameValue = req.body.usernameValue;
+    const passwordValue = req.body.passwordValue;
+    var userData;
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'whisperdb'
+    });
+
+    connection.connect((err) => {
+        if (err) {
+            console.error('Error connecting to database: ' + err.stack);
+            resObject.error = "Error connecting to the database";
+            res.status(500).json(resObject);
+            return;
+        }
+        console.log('Connected to database with connection id ' + connection.threadId);
+
+        connection.query('SELECT * FROM users WHERE username = ? AND passw = ?', [usernameValue, passwordValue], (error, results) => {
+            if (error) {
+                console.log('Error ' + error.stack);
+                res.status(500).json({ success: false, error: error.stack });
+                return;
+            }
+            if (results.length > 0) {
+                userData = results[0];
+                res.status(200).json({ success: true, userData });
+            }
+            else {res.status(200).json({ success: false, error: "No user has been found!" });}
+        });
+    });
+});
+
+app.listen(port, () => console.log("Listening on port " + port));
